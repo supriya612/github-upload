e6aab4274e6a60644be9eae321c2b15230e3ccc9
Supriya Amol Yadav <pawarsupriya45@gmail.com>
2021-12-29 08:25:46 +0530

    Add files via upload

---
+++ Datatype.java
@@ -0,0 +1,29 @@
+//data types in java
+public class Datatype
+{
+    public static void main(String arg[])
+    {
+        int studentAge=15;
+        double studentGPA=3.45;
+       
+        
+        boolean hasPerfectAttendance=true;
+        String studentFirstName="Kayla";
+        String studentLastName="Hammon";
+        char studentFirstInitial=studentFirstName.charAt(0);
+        char studentLastInitial=studentLastName.charAt(0);
+        System.out.println(studentAge);
+          System.out.println(studentGPA);
+         
+          System.out.println(hasPerfectAttendance);
+          System.out.println(studentFirstName);
+          System.out.println(studentLastName);
+          System.out.println(studentFirstInitial);
+          System.out.println(studentLastInitial);
+
+
+     
+        
+
+    }
+}
